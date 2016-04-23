0d06856f712c5fe20a97ea4d087792c5d8d134ca
Andrew Page <andrew.page32@gmail.com>
2016-04-23 19:35:39 -0400

    Code cleanup and formatting

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -1,30 +1,47 @@
 import './home.html';
 
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session';
+import { Router } from 'meteor/iron:router';
 
+/*
+ * Event helpers
+ */
 function onFlameClicked(evt, tpl)  {
   if(Session.get( 'section-clicked' ) === false) {
     Session.set( 'section-clicked', true );
-    tpl.$('.white-circle').removeClass('hidden');
-    tpl.$('.white-circle').addClass('animated');
-    tpl.$('.white-circle').addClass('bounceInUp');
+    tpl.$('.white-circle').removeClass('hidden').addClass('animated bounceInUp');
   }
+}
+
+function onCircleClicked(evt, tpl)  {
+  tpl.$('#home_page').removeClass('animated fadeIn zoomIn').addClass('animated fadeOut');
+  
+  const loadPageAfterFade = function() {
+    const path = (!evt.currentTarget || !evt.currentTarget.pathname) ? '/contact': evt.currentTarget.pathname;
+    Router.go(path);
+    $('body').removeClass('hide-scroll');
+  };
+  
+  Meteor.setTimeout( loadPageAfterFade, 1000 );
+  return false;
 }
 
 Template.home_page.events({
   'click .flame-container' : onFlameClicked,
+  'click .white-circle' : onCircleClicked,
 });
-
+/*
+ * Templete onRendered helper
+ */
 function onHomePageRendered()  {
   if(Session.get( 'section-clicked' ) === true) {
-    this.$('#flame').removeClass('animated');
-    this.$('#flame').removeClass('fadeIn');
+    this.$('#flame').removeClass('animated fadeIn');
     this.$('.white-circle').removeClass('hidden');
-    this.$('#home_page').addClass('animated');
-    this.$('#home_page').addClass('fadeIn');
-    this.$('#home_page').addClass('zoomIn');
+    this.$('#home_page').addClass('animated fadeIn zoomIn');
   }
+  $('body').addClass('hide-scroll');
 }
 
 Template.home_page.onRendered( onHomePageRendered );
