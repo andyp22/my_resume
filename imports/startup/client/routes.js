29821e047d684ada4dfad6ce686a0ff63d01a41b
Andrew Page <andrew.page32@gmail.com>
2016-04-23 17:53:40 -0400

    Add routes to man, computer, cookbook, and contact pages

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,7 +2,12 @@ import { Router } from 'meteor/iron:router';
 
 // Import to load these templates
 import '../../ui/layouts/app-body.js';
+import '../../ui/layouts/home-body.js';
 import '../../ui/pages/home.js';
+import '../../ui/pages/man.js';
+import '../../ui/pages/cookbook.js';
+import '../../ui/pages/computer.js';
+import '../../ui/pages/contact.js';
 
 /*
  * Router settings for the iron:router package.
@@ -15,6 +20,43 @@ Router.route('home', {
   name: 'home',
   path: '/',
   template: 'home_page',
+  layoutTemplate: 'HomePageLayout',
+});
+
+Router.route('man', {
+  name: 'man',
+  path: '/man',
+  template: 'man_page',
+  yieldRegions: {
+    ApplicationHeader: { to: 'header' },
+    ApplicationFooter: { to: 'footer' },
+  },
+});
+
+Router.route('cookbook', {
+  name: 'cookbook',
+  path: '/cookbook',
+  template: 'cookbook_page',
+  yieldRegions: {
+    ApplicationHeader: { to: 'header' },
+    ApplicationFooter: { to: 'footer' },
+  },
+});
+
+Router.route('computer', {
+  name: 'computer',
+  path: '/computer',
+  template: 'computer_page',
+  yieldRegions: {
+    ApplicationHeader: { to: 'header' },
+    ApplicationFooter: { to: 'footer' },
+  },
+});
+
+Router.route('contact', {
+  name: 'contact',
+  path: '/contact',
+  template: 'contact_page',
   yieldRegions: {
     ApplicationHeader: { to: 'header' },
     ApplicationFooter: { to: 'footer' },
