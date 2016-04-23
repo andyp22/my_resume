d74c352e473444cc308cba25dc29d012c09dc4b6
Andrew Page <andrew.page32@gmail.com>
2016-04-23 17:51:13 -0400

    Add animate.css library

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 import '../imports/ui/stylesheets/bootstrap-settings.json';
+import '../imports/ui/stylesheets/vendor/animate.css';
 import '../imports/startup/client/template-helpers.js';
 import '../imports/startup/client/routes.js';
 import '../imports/startup/client/index.js';
