2fbb8c194229dfe989e6b29f41b512a58051c435
Andrew Page <andrew.page32@gmail.com>
2016-04-23 19:01:06 -0400

    Change code to use flame component

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -4,12 +4,27 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 function onFlameClicked(evt, tpl)  {
-  console.log('click');
-  tpl.$('.white-circle').removeClass('hidden');
-  tpl.$('.white-circle').addClass('animated');
-  tpl.$('.white-circle').addClass('bounceInUp');
+  if(Session.get( 'section-clicked' ) === false) {
+    Session.set( 'section-clicked', true );
+    tpl.$('.white-circle').removeClass('hidden');
+    tpl.$('.white-circle').addClass('animated');
+    tpl.$('.white-circle').addClass('bounceInUp');
+  }
 }
 
 Template.home_page.events({
   'click .flame-container' : onFlameClicked,
 });
+
+function onHomePageRendered()  {
+  if(Session.get( 'section-clicked' ) === true) {
+    this.$('#flame').removeClass('animated');
+    this.$('#flame').removeClass('fadeIn');
+    this.$('.white-circle').removeClass('hidden');
+    this.$('#home_page').addClass('animated');
+    this.$('#home_page').addClass('fadeIn');
+    this.$('#home_page').addClass('zoomIn');
+  }
+}
+
+Template.home_page.onRendered( onHomePageRendered );
