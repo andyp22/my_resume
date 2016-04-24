7bba577fa0814fd5fe9374a1cfa0795b8fe307c8
Andrew Page <andrew.page32@gmail.com>
2016-04-24 03:37:18 -0400

    Add cookbook layout

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'meteor/iron:router';
 // Import to load these templates
 import '../../ui/layouts/app-body.js';
 import '../../ui/layouts/home-body.js';
+import '../../ui/layouts/cookbook-body.js';
 import '../../ui/pages/home.js';
 import '../../ui/pages/man.js';
 import '../../ui/pages/cookbook.js';
@@ -36,11 +37,11 @@ Router.route('man', {
 Router.route('cookbook', {
   name: 'cookbook',
   path: '/cookbook',
-  template: 'cookbook_page',
-  yieldRegions: {
-    ApplicationHeader: { to: 'header' },
-    ApplicationFooter: { to: 'footer' },
-  },
+  template: 'cookbookPage',
+  layoutTemplate: 'CookbookPageLayout',
+  data: {
+    sections: cookbookData.sections,
+  }
 });
 
 Router.route('computer', {
---
+++ imports/ui/layouts/cookbook-body.js
@@ -0,0 +1,1 @@
+import './cookbook-body.html';
