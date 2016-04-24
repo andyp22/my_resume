a368d1cab511122add46e42b5764c72c7ddb7ec7
Andrew Page <andrew.page32@gmail.com>
2016-04-24 03:38:51 -0400

    Add basic parallax functionality

--- imports/ui/pages/cookbook.js
+++ imports/ui/pages/cookbook.js
@@ -1,4 +1,54 @@
 import './cookbook.html';
+import '../data/cookbook.js';
+import '../components/viewportSize.js';
+
+import skrollr from 'skrollr';
+import imagesloaded from 'imagesloaded';
 
 // import { Template } from 'meteor/templating';
 // import { ReactiveVar } from 'meteor/reactive-var';
+
+function onSkrollrRendered()  {
+  // Setup variables
+  $window = this.$(window);
+  $slide = this.$('.homeSlide');
+  $slideTall = this.$('.homeSlideTall');
+  $slideTall2 = this.$('.homeSlideTall2');
+  $cookbook = this.$('#cookbook');
+  
+  //FadeIn all sections   
+  $cookbook.imagesLoaded( function() {
+    setTimeout(function() {
+      // Resize sections
+      adjustWindow();
+      // Fade in sections
+      $cookbook.removeClass('loading').addClass('loaded');
+    }, 800);
+  });
+  
+  function adjustWindow(){
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            console.log(data.curTop);
+        }
+    });
+    
+    // Get window size
+    winH = $window.height();
+    // Keep minimum height 550
+    if(winH <= 550) {
+      winH = 550;
+    } 
+      
+    // Resize our slides
+    $slide.height(winH);
+    $slideTall.height(winH*2);
+    $slideTall2.height(winH*3);
+    // Refresh Skrollr after resizing our sections
+    s.refresh($('.homeSlide'));
+  }
+}
+
+Template.cookbookPage.onRendered(onSkrollrRendered);
