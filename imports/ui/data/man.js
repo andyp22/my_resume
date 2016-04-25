bd3f43dd2e2e9d04ecdf06d6126ea2ec6a01121f
Andrew Page <andrew.page32@gmail.com>
2016-04-25 02:14:08 -0400

    Add man data

---
+++ imports/ui/data/man.js
@@ -0,0 +1,247 @@
+manData = {
+  name: 'Andrew Page',
+  location: {
+    city: 'Charlevoix',
+    state: 'Michigan',
+    country: 'USA',
+    zipCode: '49720',
+  },
+  sex: 'Male',
+  hair: 'Magnificent',
+  eyes: 'Deep and dark',
+  nationality: 'American',
+  dob: 'unknown',
+  email: 'andrew.page32@gmail.com',
+  blog: 'andypangus.com',
+  github: 'https://github.com/andyp22/',
+  actionShotUrl: '/images/cookbook/pateencroute.jpg',
+  skills: [
+    {
+      name: 'JavaScript',
+      category: 'web, language',
+    },
+    {
+      name: 'Node.js',
+      category: 'web, library',
+    },
+    {
+      name: 'Meteor.js',
+      category: 'web, library, framework',
+    },
+    {
+      name: 'jQuery',
+      category: 'web, library',
+    },
+    {
+      name: 'Backbone.js',
+      category: 'web, library',
+    },
+    {
+      name: 'Less.js',
+      category: 'web, library, style',
+    },
+    {
+      name: 'Handlebars.js',
+      category: 'web, library, templates',
+    },
+    {
+      name: 'Spacebars',
+      category: 'web, library, templates',
+    },
+    {
+      name: 'HTML',
+      category: 'web, markup',
+    },
+    {
+      name: 'CSS',
+      category: 'web, style',
+    },
+    {
+      name: 'PHP',
+      category: 'web, language',
+    },
+    {
+      name: 'C#',
+      category: 'language',
+    },
+    {
+      name: 'Unity3D',
+      category: 'framework',
+    },
+    {
+      name: 'JSON',
+      category: 'data, web',
+    },
+    {
+      name: 'XML',
+      category: 'data, web',
+    },
+    {
+      name: 'Flash/ActionScript',
+      category: 'web, language',
+    },
+    {
+      name: 'Drupal',
+      category: 'framework, cms',
+    },
+    {
+      name: 'Wordpress',
+      category: 'framework, cms',
+    },
+    {
+      name: 'ANT',
+      category: 'automation',
+    },
+    {
+      name: 'Python',
+      category: 'automation, language',
+    },
+    {
+      name: 'JAVA',
+      category: 'language',
+    },
+    {
+      name: 'SQL',
+      category: 'data, web',
+    },
+    {
+      name: 'MongoDB',
+      category: 'data, web',
+    },
+    {
+      name: 'Unit Testing',
+      category: 'quality',
+    },
+    {
+      name: 'Integration Testing',
+      category: 'quality',
+    },
+    {
+      name: 'Windows',
+      category: 'platform',
+    },
+    {
+      name: 'Mac OSX/Unix',
+      category: 'platform',
+    },
+    {
+      name: 'Subversion',
+      category: 'version control',
+    },
+    {
+      name: 'Git',
+      category: 'version control',
+    },
+    {
+      name: 'SCORM',
+      category: 'lms',
+    },
+    {
+      name: 'AICC',
+      category: 'lms',
+    },
+    {
+      name: 'AWS',
+      category: 'web, cloud',
+    }
+  ],
+  missions: [
+    {
+      name: 'Senior Developer',
+      organization: 'Enspire',
+      location: 'Austin, TX',
+      dates: {
+        start: '10/2009',
+        end: 'Currently Active',
+      },
+      tasks: [
+        'Creates interactive online training and educational experiences',
+        'Integrates courseware with various Learning Management Systems',
+        'Lead project tech on some of the biggeset projects at Enspire',
+        'Collaborates with clients to ensure project techincal goals align with client architecture, integration strategy, and validation of project deliverables',
+        'Improves productivity by innovating new tools and developing existing tools',
+        'Provides technical sales support',
+        'Numerous engagements including: AACN, AHA, AMD, Dell, NHA, PRI of NY, and Prudential'
+      ],
+      description: 'Currently working in the online training and eductional sphere, Andrew is a key member of Enspire\'s team. Working with a variety of technologies including Node.js, JavaScript, HTML/CSS, Meteor, Java, MongoDB, and others keeps his mind quick and his fingers quicker. Andrew has a proven ability to communicate and deliver consistently on projects of all sizes.',
+    },
+    {
+      name: 'Web Manager',
+      organization: 'Park Place Publications',
+      location: 'Austin, TX',
+      dates: {
+        start: '08/2008',
+        end: '09/2009',
+      },
+      tasks: [
+        'Unified the web presence for both of PPP\'s major products',
+        'Defined and implemented a process for converting Word manuscripts to eBooks',
+        'Built microsites for books and authors distributed by PPP'
+      ],
+      description: 'During his time at Park Place, Andrew was pivotal in unifying PPP\'s web presence and helping their business embrace the web as means to grow.',
+    },
+    {
+      name: 'Freelance Developer',
+      location: 'Austin, TX',
+      dates: {
+        start: '01/2007',
+        end: '07/2008',
+      },
+      tasks: [
+        'Built websites based on client specifications',
+        'Built out new features for existing sites',
+      ],
+      description: 'Andrew worked as a freelance web developer while taking classes at ACC.',
+    }
+  ],
+  training: [
+    {
+      name: 'New England Culinary Institute',
+      studied: 'Culinary Arts',
+      degree: 'AOS',
+      attended: '1996-1998',
+    },
+    {
+      name: 'Austin Community College',
+      studied: 'Computer Science',
+      attended: '2005-2009',
+    }
+  ],
+  projects: [
+    {
+      name: 'Hunting Turn-based Strategy/RPG',
+      description: 'A computer game built using C# in Unity3D where the player uses skills like tracking, shooting, and stalking to hunt animals in various settings. Features procedural terrain, water bodies, and forest generation on a hexagonal grid with turn-based movement. Still in active development. ',
+      links: [
+        {
+          url: 'https://github.com/andyp22/unity_gov_game',
+          text: 'Locked - Requires Special Clearance',
+          label: 'Source Code'
+        },
+        {
+          url: 'https://www.youtube.com/playlist?list=PLl35Zi9-ZP3NCELzVYWbhSfF4yu0Ycju7',
+          text: 'Surveillance Footage',
+          label: 'Video'
+        }
+      ]
+    },
+    {
+      name: 'DPLA Card Catalogue',
+      description: 'A web application which interfaces with the Digital Public Library of America\'s collection of digital books, images, audio, and video through their external search API.',
+      links: [
+        {
+          url: 'https://github.com/andyp22/meteor-dpla',
+          text: 'Leaked Source',
+          label: 'Source'
+        }
+      ]
+    }
+  ],
+  poi: [  // points of interest
+    'Former Chef, Pastry Chef, & Restaurant Owner',
+    'Gardener',
+    'Plays the Frech Horn',
+    'Colonial American history buff',
+    'Loves Soccer',
+    'Avid Camper and Hiker',
+  ]
+};
