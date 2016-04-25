2b333b3ec68c5d2feda4b84083c8df19cfa0f045
Andrew Page <andrew.page32@gmail.com>
2016-04-25 15:44:25 -0400

    Add basic cleverbot integration

---
+++ imports/startup/server/chatbot.js
@@ -0,0 +1,57 @@
+
+import { Meteor } from 'meteor/meteor';
+import cleverbot from 'cleverbot.io';
+import Future from 'fibers/future';
+
+let cbuid = '';
+if (Meteor.settings) {
+  if (Meteor.settings.private) {
+    if (Meteor.settings.private.cleverbot_userid) {
+      cbuid = Meteor.settings.private.cleverbot_userid;
+    }
+  }
+}
+
+let cbapi = '';
+if (Meteor.settings) {
+  if (Meteor.settings.private) {
+    if (Meteor.settings.private.cleverbot_api_key) {
+      title = Meteor.settings.private.cleverbot_api_key;
+    }
+  }
+}
+
+const bot = new cleverbot(cbuid, cbapi);
+
+function onCallingChatbot(userId)  {
+  var fut = new Future();
+  bot.setNick(userId);
+  bot.create(function(err, session) {
+    if (err) {
+      return fut.throw(err);
+    } else {
+      return fut.return(session);
+    }
+  });
+  
+  return fut.wait();
+}
+
+function onAskingChatbot(message)  {
+  var fut = new Future();
+  
+  bot.ask(message, function(err, response) {
+    if (err) {
+      return fut.throw(err);
+    } else {
+      return fut.return(response);
+    }
+  });
+  
+  return fut.wait();
+}
+
+Meteor.methods({
+  callingChatbot: onCallingChatbot,
+  askingChatbot: onAskingChatbot,
+});
--- imports/ui/pages/computer.js
+++ imports/ui/pages/computer.js
@@ -1,4 +1,48 @@
 import './computer.html';
 
-// import { Template } from 'meteor/templating';
-// import { ReactiveVar } from 'meteor/reactive-var';
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+import { Session } from 'meteor/session';
+
+Session.setDefault('session-nick', '');
+
+if (Session.equals('session-nick', '')) {
+  Session.set('session-nick', Random.id());
+}
+
+function callingChatbotCallback(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    if(!res) {
+      console.log("Chat session already started.");
+    } else {
+      console.log(err, res);
+    }
+  }
+}
+
+function askingChatbotCallback(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res);
+  }
+}
+
+function onCallButtonClicked(evt, tpl) {
+  evt.preventDefault();
+  
+  Meteor.call('askingChatbot', 'Is there anybody out there?', askingChatbotCallback);
+  
+}
+
+Template.computerPage.events({
+  'click #callButton': onCallButtonClicked,
+});
+
+function onComputerPageRendered() {
+  Meteor.call('callingChatbot', Session.get('session-nick'), callingChatbotCallback);
+}
+
+Template.computerPage.onRendered(onComputerPageRendered);
