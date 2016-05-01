b535ccd25483ba6d51cf2d981f6287ab38a37fc7
Andrew Page <andrew.page32@gmail.com>
2016-05-01 18:12:34 -0400

    Change page transition animations

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -1,48 +1,18 @@
 import './home.html';
 
-import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
 import { $ } from 'meteor/jquery';
-import { Router } from 'meteor/iron:router';
 
-Session.setDefault('section-clicked', false);
+Session.setDefault('session-started', false);
 
-/*
- * Event helpers
- */
-function onFlameClicked(evt, tpl) {
-  if (Session.equals('section-clicked', false)) {
-    Session.set('section-clicked', true);
-    tpl.$('.white-circle').removeClass('hidden').addClass('animated bounceInUp');
-  }
-}
-
-function onCircleClicked(evt, tpl) {
-  tpl.$('#home_page').removeClass('animated fadeIn zoomIn').addClass('animated fadeOut');
-
-  function loadPageAfterFade() {
-    const path = (!evt.currentTarget.pathname) ? '/contact' : evt.currentTarget.pathname;
-    Router.go(path);
-    $('body').removeClass('hide-scroll');
-  }
-
-  Meteor.setTimeout(loadPageAfterFade, 1000);
-  return false;
-}
-
-Template.homePage.events({
-  'click .flame-container': onFlameClicked,
-  'click .white-circle': onCircleClicked,
-});
 /*
  * Templete onRendered helper
  */
 function onHomePageRendered() {
-  if (Session.equals('section-clicked', true)) {
-    this.$('#flame').removeClass('animated fadeIn');
-    this.$('.white-circle').removeClass('hidden');
-    this.$('#home_page').addClass('animated fadeIn zoomIn');
+  if (Session.equals('session-started', false)) {
+    Session.set('session-started', true);
+    $('#flame').addClass('animated fadeIn');
   }
   $('body').addClass('hide-scroll');
 }
