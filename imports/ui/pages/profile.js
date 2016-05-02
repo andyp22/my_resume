72f4b97624358b6df88a19e9f5c0a9a0f139c12f
Andrew Page <andrew.page32@gmail.com>
2016-05-02 14:14:37 -0400

    Add styling and format user data

--- imports/ui/pages/profile.js
+++ imports/ui/pages/profile.js
@@ -1,1 +1,37 @@
 import './profile.html';
+
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Session } from 'meteor/session';
+
+Session.setDefault('', );
+
+let templateName = new ReactiveVar('ProfileStats');
+
+Template.profileView.helpers({
+  templateName: function()  {
+    return templateName.get();
+  }
+});
+
+Template.ProfileStats.helpers({
+  handle: function()  {
+    return Meteor.user().profile.name;
+  },
+  email: function()  {
+    return Meteor.user().emails[0].address;
+  }
+});
+
+Template.ProfileActionShot.helpers({
+  actionShotUrl: function()  {
+    return "/images/mystery_man.png";
+  }
+});
+
+Template.UserProgression.helpers({
+  profile: function()  {
+    return Meteor.user().profile;
+  }
+});
