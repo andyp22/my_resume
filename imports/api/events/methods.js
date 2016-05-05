17075628da96d6a0523205d49c4567826660f345
Andrew Page <andrew.page32@gmail.com>
2016-05-04 23:15:33 -0400

    Add new methods for dealing with events and categories

--- imports/api/events/methods.js
+++ imports/api/events/methods.js
@@ -6,45 +6,69 @@ import { Roles } from 'meteor/alanning:roles';
 
 import { Events } from './events.js';
 
-const NAME_AND_USERID_ONLY = new SimpleSchema({
+const NAME_USERID_AND_CAT = new SimpleSchema({
   name: { type: String },
   userId: { type: String },
+  category: { type: String, optional: true, defaultValue: 'global' }
 }).validator();
 
-export const insert = new ValidatedMethod({
+const USERID_AND_CAT_ONLY = new SimpleSchema({
+  userId: { type: String },
+  category: { type: String },
+}).validator();
+
+export const insertEvent = new ValidatedMethod({
   name: 'Events.methods.insert',
   validate: Events.schema.validator(),
-  run(newEvent) {
-    return Events.insert(newEvent);
+  run({ name, userId, category}) {
+    const ue = Events.findOne({ name: name, userId: userId, reset: false });
+    if(!ue) {
+      return Events.insert({ name: name, userId: userId, category: category});
+    } else {
+      return 'An event by this name already exists.';
+    }
   },
 });
 
 export const resetEvent = new ValidatedMethod({
   name: 'Events.methods.updateReset',
-  validate: NAME_AND_USERID_ONLY,
-  run({ name, userId }) {
+  validate: NAME_USERID_AND_CAT,
+  run({ name, userId, category}) {
     const userEvents = Events.find({ name: name, userId: userId, reset: false });
-    let updatedEvents = { ids: [] };
-    if(userEvents.count() > 0)  {
-      userEvents.forEach(function(userEvent, index, cursor) {
-        if(userEvent.editableBy(userId) || Roles.userIsInRole(this.userId, ['admin'], Roles.GLOBAL_GROUP))  {
-          Events.update(userEvent._id, {
-            $set: { reset: true, resetAt: new Date() },
-          });
-          updatedEvents.ids.push(userEvent._id);
-        } else {
-          throw new Meteor.Error('Events.methods.updateReset',
-            'Cannot update the last event.');
-        }
-      });
-    }
-    return updatedEvents;
+    return resetEventsList(userEvents);;
   },
 });
 
+export const resetCategory = new ValidatedMethod({
+  name: 'Events.methods.resetCategory',
+  validate: USERID_AND_CAT_ONLY,
+  run({ userId, category }) {
+    const userEvents = Events.find({ userId: userId, category: category, reset: false });
+    return resetEventsList(userEvents);
+  },
+});
+
+function resetEventsList(userEvents)  {
+  let updatedEvents = { ids: [] };
+  if(userEvents.count() > 0)  {
+    userEvents.forEach(function(userEvent, index, cursor) {
+      if(userEvent.editableBy(userEvent.userId) || Roles.userIsInRole(this.userId, ['admin'], Roles.GLOBAL_GROUP))  {
+        Events.update(userEvent._id, {
+          $set: { reset: true, resetAt: new Date() },
+        });
+        updatedEvents.ids.push(userEvent._id);
+      } else {
+        throw new Meteor.Error('Events.methods.updateReset',
+          'Cannot update the last event.');
+      }
+    });
+  }
+  return updatedEvents;
+}
+
 export const removeEvent = new ValidatedMethod({
   name: 'Events.methods.removeEvent',
-  validate: NAME_AND_USERID_ONLY,
+  validate: NAME_USERID_AND_CAT,
   run({ name, userId }) {
     if (Roles.userIsInRole(this.userId, ['admin'], Roles.GLOBAL_GROUP)) {
       const userEvents = Events.find({ name: name, userId: userId });
@@ -62,8 +86,9 @@ export const removeEvent = new ValidatedMethod({
 
 // Get list of all method names on Lists
 const EVENTS_METHODS = _.pluck([
-  insert,
+  insertEvent,
   resetEvent,
+  resetCategory,
   removeEvent,
 ], 'name');
 
