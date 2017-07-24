0e0c202e8ce7e2bc971fecd39edafa37be355f90
Andrew Page <andrew.page32@gmail.com>
2017-07-23 23:04:08 -0400

    Enabling some server startup scripts

--- imports/startup/server/index.ts
+++ imports/startup/server/index.ts
@@ -1,9 +1,9 @@
-//import '/imports/api/logger/init';
-//import './roles';
-//import './admin';
-//import './register-api';
-//import './email';
-//import './fixtures';
+import '/imports/api/logger/init';
+import './roles';
+import './admin';
+import './register-api';
+import './email';
+import './fixtures';
 //import './chatbot';
-//import './services';
-//import './accounts';
+import './services';
+import './accounts';
