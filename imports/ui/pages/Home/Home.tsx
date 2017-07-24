10aa648624c112f17ed8bdfdd2ac7f147c511ac9
Andrew Page <andrew.page32@gmail.com>
2017-07-24 00:31:16 -0400

    Add main nav links

--- imports/ui/pages/Home/Home.tsx
+++ imports/ui/pages/Home/Home.tsx
@@ -1,7 +1,8 @@
 require('./Home.scss');
 
 import * as React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Card, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 interface HomeProps { }
 interface HomeState { }
@@ -11,7 +12,23 @@ export class HomeComponent extends React.Component<HomeProps, HomeState> {
     const props = this.props;
     return (
       <Container className="home-page page">
-        <Header as="h1">This is the home page</Header>
+        <Container className="home-navigation" role="navigation" aria-label="Home Page Links">
+          <Card>
+            <Card.Content>
+              <Link to="/man"><Image src="/images/man.png" alt="Man" /></Link>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Link to="/cookbook"><Image src="/images/cookbook.png" alt="Cookbook" /></Link>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Link to="/computer"><Image src="/images/computer.png" alt="Computer" /></Link>
+            </Card.Content>
+          </Card>
+        </Container>
       </Container>
     );
   }
