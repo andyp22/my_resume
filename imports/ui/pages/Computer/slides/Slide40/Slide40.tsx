d6d5ad31f6e338809832713e605048096a173b8d
Andrew Page <andrew.page32@gmail.com>
2017-08-08 21:33:01 -0400

    Add slide 40 content

--- imports/ui/pages/Computer/slides/Slide40/Slide40.tsx
+++ imports/ui/pages/Computer/slides/Slide40/Slide40.tsx
@@ -10,18 +10,11 @@ import { SlideComponent } from '../../components/Slide';
 class Slide40Component extends SlideComponent {
   render() {
     return (
-      <div className={this.getComponentClasses('story-slide-content-9')}>
-        <div className="background-layer">
-          <div className="table-bg " style={this.getBackgroundImageStyle('table-background.png', '/images/story/part1')} />
-        </div>
-        <div className="midground-layer">
-          <div className="whiskey-bottle " style={this.getBackgroundImageStyle('whiskey.png', '/images/story/part1')} />
-          <div className="adell alphadOut" style={this.getBackgroundImageStyle('Adell.png', '/images/story/part1')} />
-          <div className="lupov facing-left alphadOut" style={this.getBackgroundImageStyle('Lupov.png', '/images/story/part1')} />
-        </div>
+      <div className={this.getComponentClasses('story-slide-content-40')}>
+        <div className="background-layer"></div>
+        <div className="midground-layer"></div>
         <div className="foreground-layer">
-          <div className="glass-1 glass " style={this.getBackgroundImageStyle('highball.png', '/images/story/part1')} />
-          <div className="glass-2 glass " style={this.getBackgroundImageStyle('highball.png', '/images/story/part1')} />
+          <div className="lupov " style={this.getBackgroundImageStyle('Lupov.png', '/images/story/part1')} />
         </div>
       </div>
     );
@@ -29,42 +22,11 @@ class Slide40Component extends SlideComponent {
 
   buildTimeline(): void {
     this.tl.fromTo(
-      document.getElementsByClassName('adell')[0],
-      0.5,
-      { autoAlpha: 0 },
-      { autoAlpha: 1 },
-      1);
-    this.tl.to(
-      document.getElementsByClassName('glass-2')[0],
-      0.5,
-      { autoAlpha: 0 },
-      1);
-    this.tl.to(
-      document.getElementsByClassName('glass-2')[0],
-      0.5,
-      { top: 340, left: 585 },
-      3);
-    this.tl.fromTo(
       document.getElementsByClassName('lupov')[0],
-      0.8,
+      0.5,
       { autoAlpha: 0 },
       { autoAlpha: 1 },
-      10);
-    this.tl.to(
-      document.getElementsByClassName('adell')[0],
-      0.8,
-      { autoAlpha: 0 },
-      10);
-    this.tl.to(
-      document.getElementsByClassName('glass-1')[0],
-      0.8,
-      { autoAlpha: 0 },
-      10);
-    this.tl.to(
-      document.getElementsByClassName('glass-2')[0],
-      0.8,
-      { autoAlpha: 1 },
-      10);
+      1);
   }
 }
 
