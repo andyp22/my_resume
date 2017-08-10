2680bcd9035ce9b9deed3e704c8c6f7546791165
Andrew Page <andrew.page32@gmail.com>
2017-08-10 01:10:22 -0400

    Update faqs

--- imports/ui/data/faq.js
+++ imports/ui/data/faq.js
@@ -11,19 +11,19 @@ export default FAQs = [
     answer: 'I am male; I was a chef in my former career and still am at home; I work with computers.',
   },
   {
-    question: 'Why do I need a user account?',
-    answer: 'You don’t, strictly speaking. My aim is to use user accounts to personalize the content and site experience. I do not sell, use, or even look at user information beyond viewing which events have been triggered and basic analytics.',
+    question: 'Are you using Celsius or Fahrenheit for temperature?',
+    answer: 'All recipes list temperatures in degrees fahrenheit.',
   },
   {
-    question: 'Events?',
-    answer: 'Different actions you take on the site are tracked as part of your user account. These are called events. In most cases triggering an event unlocks more content and/or the next step in whatever you are doing.',
+    question: 'Pee-cans? Puh-cans? Puh-cahns?',
+    answer: 'A pee-can is what you use to go to the bathroom. I have no idea what a puh-can is. The proper way to say the word is puh-cahn.',
   },
   {
-    question: 'What if I just want to see all the content on your site and not use the event system?',
-    answer: 'For certain sections of the site, that are tied into events, there should be user controls which allow you to unlock the content. Other sections will need to be viewed in order by design. You can also reset your events and start over through your user profile.',
+    question: 'What is the tech stack you are using?',
+    answer: 'Meteor, React, TypeScript, SASS',
   },
   {
     question: 'Where can I see the code behind the site?',
-    answer: 'On my Github here: <a href="https://github.com/andyp22/my_resume" target="_blank">https://github.com/andyp22/my_resume</a> I try to work on it when I can but adulting is a time consuming thing and I don’t always get to work on what I want when I want.',
+    answer: 'On my Github here: <a href="https://github.com/andyp22/my_resume" target="_blank">https://github.com/andyp22/my_resume</a>',
   },
 ];
